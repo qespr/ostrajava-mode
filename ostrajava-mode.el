8d9d464e953504d6b7292c1b914d094a7e28cc8e
qespr <rspointer@post.cz>
2022-12-16 20:17:01 +0100

    Základní barvení syntaxe a ukázkový soubor

--- ostrajava-mode.el
+++ ostrajava-mode.el
@@ -20,11 +20,51 @@
 
 ;;; Commentary:
 
-;; 
+;; Ostravsky neumím takže musím psát česky, bohužel
 
 ;;; Code:
+;;Hlavní zdroj: http://www.xahlee.info/emacs/emacs/elisp_syntax_coloring.html
+(setq ostrajava-font-lock-keywords
+      (let* (
+            ;; define several category of keywords
+             (x-keywords '("banik" "fajront" ;;Značení souboru
+			   "tryda" "joch" "fagan od" "forant" "zrob" ;;OOP
+			   "kajtez" "boinak" "kaj" ;;podmínky
+			   "rubat" "zdybat" "dlabat" ;;cykly
+			   "davaj"
+			   "ci" "aj" ;;Logické operace
+			   "toz" ;;Deklarace
+			   ))
+             (x-types '("cyslo" "cyslo_desetinne" "bul" "chachar" ;Primitivy
+			"Dryst" "Bazmek" "Citac" "Konzola" "Bafr")) ;;Třídy
+             (x-constants '("fajne" "nyt" "chuj"))
+             (x-events '("Ostrava" "pyco"))
+             (x-functions '("naDryst" "kantuje" ;;Funkce děděné z Objektu
+			    "citajRadku" "otevr" "zavr" ;;Funkce v Citaci
+			    "rynek"))
 
+            ;; generate regex string for each category of keywords
+            (x-keywords-regexp (regexp-opt x-keywords 'words))
+            (x-types-regexp (regexp-opt x-types 'words))
+            (x-constants-regexp (regexp-opt x-constants 'words))
+            (x-events-regexp (regexp-opt x-events 'words))
+            (x-functions-regexp (regexp-opt x-functions 'words)))
 
+        `(
+          (,x-types-regexp . 'font-lock-type-face) ;; 'font-lock--face
+          (,x-constants-regexp . 'font-lock-constant-face)
+          (,x-events-regexp . 'font-lock-builtin-face)
+          (,x-functions-regexp . 'font-lock-function-name-face)
+          (,x-keywords-regexp . 'font-lock-keyword-face)
+          ;; note: order above matters, because once colored, that part won't change.
+          ;; in general, put longer words first
+          )))
+
+(define-derived-mode ostrajava-mode java-mode "ostrajava"
+  "Major mode for editing OSTRAjava files"
+  (setq font-lock-defaults '((ostrajava-font-lock-keywords))))
+
+(add-to-list 'auto-mode-alist '("\\.cyp\\'" . ostrajava-mode))
 
 (provide 'ostrajava-mode)
 ;;; ostrajava-mode.el ends here
---
+++ res/ukazka.cyp
@@ -0,0 +1,45 @@
+/*
+  soubor převzat z repa OSTRAjavy: https://github.com/tkohout/OSTRAJava/blob/master/examples/Prints/Prints.cyp
+ */
+
+banik pyco
+
+tryda A {
+    toz cyslo c pyco
+
+    A(cyslo c){
+        joch.c = c pyco
+    }
+
+    Dryst naDryst(){
+        toz CysloCele cele = zrob CysloCele(joch.c) pyco
+        davaj cele.naDryst() pyco
+    }
+}
+
+tryda Ostrava{
+
+    rynek(){
+        toz Dryst dryst1 = "Nejaky" pyco
+        toz Dryst dryst2 = " dryst" pyco
+
+        toz cyslo i = 250 pyco
+        toz chachar c = 'b' pyco
+        toz bul b = fajne pyco
+        toz A a = zrob A(1020) pyco
+
+        Konzola.pravit(dryst1.chacharNa(2)) pyco
+        Konzola.pravit(dryst1.pridaj(dryst2)) pyco
+
+        Konzola.pravit(a) pyco
+
+        //Mel by zavolat naDryst()
+        Konzola.pravit(a) pyco
+        Konzola.pravit(i) pyco
+        Konzola.pravit(c) pyco
+        Konzola.pravit(b) pyco
+    }
+
+}
+
+fajront pyco
